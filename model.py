7b15456be36c8def1f61ade2f5f8164326765c87
Luis Elías Carro <lcarro@globalnewsgroup.com>
2021-11-23 16:06:34 -0300

    commit 20211123 -4
    corrección algunos bugs

--- model.py
+++ model.py
@@ -58,32 +58,29 @@ class Model:
         try:
             db_cacho = sqlite3.connect('carro_maier.db')
 
-            try:
-                csr_cacho = db_cacho.cursor()
+            csr_cacho = db_cacho.cursor()
 
-                sql_drop = "DROP TABLE IF EXISTS `Noticias`"
-                sql_create = """
-                    CREATE TABLE `Noticias`(
-                        Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                        Fecha DATE,
-                        Medio VARCHAR(128) COLLATE NOCASE NOT NULL,
-                        Seccion VARCHAR(128) COLLATE NOCASE NOT NULL,
-                        Titulo VARCHAR(128) COLLATE NOCASE NOT NULL,
-                        Cuerpo TEXT COLLATE NOCASE NOT NULL
-                        )
-                    """
-                csr_cacho.execute(sql_drop)
-                csr_cacho.execute(sql_create)
-                db_cacho.commit()
-                db_cacho.close()
-
-            except Exception as e:
-                db_cacho.rollback()
-                db_cacho.close()
-                raise Exception(f"error al crear tabla `Noticias`: {str(e)}")
+            sql_drop = "DROP TABLE IF EXISTS `Noticias`"
+            sql_create = """
+                CREATE TABLE `Noticias`(
+                    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    Fecha DATE,
+                    Medio VARCHAR(128) COLLATE NOCASE NOT NULL,
+                    Seccion VARCHAR(128) COLLATE NOCASE NOT NULL,
+                    Titulo VARCHAR(128) COLLATE NOCASE NOT NULL,
+                    Cuerpo TEXT COLLATE NOCASE NOT NULL
+                    )
+                """
+            csr_cacho.execute(sql_drop)
+            csr_cacho.execute(sql_create)
+            db_cacho.commit()
 
         except Exception as e:
-            raise Exception(f"error al abrir base de datos carro_maier: {str(e)}")
+            db_cacho.rollback()
+            raise Exception(f"error al crear tabla `Noticias`: {str(e)}")
+
+        finally:
+            db_cacho.close()
 
     def save_data(self, noticia):
         """
@@ -92,44 +89,41 @@ class Model:
         try:
             db_cacho = sqlite3.connect('carro_maier.db')
 
-            try:
-                csr_cacho = db_cacho.cursor()
+            csr_cacho = db_cacho.cursor()
 
-                medio = re.sub("[\"']", r"", noticia.medio)
-                seccion = re.sub("[\"']", r"", noticia.seccion)
-                titulo = re.sub("[\"']", r"", noticia.titulo)
-                cuerpo = re.sub("[\"']", r"", noticia.cuerpo)
+            medio = re.sub("[\"']", r"", noticia.medio)
+            seccion = re.sub("[\"']", r"", noticia.seccion)
+            titulo = re.sub("[\"']", r"", noticia.titulo)
+            cuerpo = re.sub("[\"']", r"", noticia.cuerpo)
 
-                if noticia.id == "0":
-                    sql_insert = """
-                        INSERT INTO Noticias (Fecha, Medio, Seccion, Titulo, Cuerpo)
-                            VALUES (?, ?, ?, ?, ?)
-                        """
-                    datos = (noticia.fecha, medio, seccion, titulo, cuerpo)
+            if noticia.id == "0":
+                sql_insert = """
+                    INSERT INTO Noticias (Fecha, Medio, Seccion, Titulo, Cuerpo)
+                        VALUES (?, ?, ?, ?, ?)
+                    """
+                datos = (noticia.fecha, medio, seccion, titulo, cuerpo)
 
-                    csr_cacho.execute(sql_insert, datos)
-                else:
-                    sql_update = f"""
-                        UPDATE Noticias SET
-                            Fecha = '{noticia.fecha}',
-                            Medio = '{medio}',
-                            Seccion = '{seccion}',
-                            Titulo = '{titulo}',
-                            Cuerpo = '{cuerpo}'
-                        WHERE Id = {noticia.id}
-                        """
-                    csr_cacho.execute(sql_update)
+                csr_cacho.execute(sql_insert, datos)
+            else:
+                sql_update = f"""
+                    UPDATE Noticias SET
+                        Fecha = '{noticia.fecha}',
+                        Medio = '{medio}',
+                        Seccion = '{seccion}',
+                        Titulo = '{titulo}',
+                        Cuerpo = '{cuerpo}'
+                    WHERE Id = {noticia.id}
+                    """
+                csr_cacho.execute(sql_update)
 
-                db_cacho.commit()
-                db_cacho.close()
-
-            except Exception as e:
-                db_cacho.rollback()
-                db_cacho.close()
-                raise Exception(f"error al {'insertar' if noticia.id == '0' else 'actualizar'} registro en tabla Noticias: {str(e)}")
+            db_cacho.commit()
 
         except Exception as e:
-            raise Exception(f"error al abrir base de datos carro_maier: {str(e)}")
+            db_cacho.rollback()
+            raise Exception(f"error al {'insertar' if noticia.id == '0' else 'actualizar'} registro en tabla Noticias: {str(e)}")
+
+        finally:
+            db_cacho.close()
 
     def get_datos(self, search_id):
         """
@@ -139,32 +133,30 @@ class Model:
         try:
             db_cacho = sqlite3.connect('carro_maier.db')
 
-            try:
-                csr_cacho = db_cacho.cursor()
-                sql_get = f"""
-                    SELECT
-                        Id
-                        , Fecha
-                        , Medio
-                        , Seccion
-                        , Titulo
-                        , Cuerpo
-                    FROM Noticias
-                    WHERE Id = {search_id}
-                    """
+            csr_cacho = db_cacho.cursor()
+            sql_get = f"""
+                SELECT
+                    Id
+                    , Fecha
+                    , Medio
+                    , Seccion
+                    , Titulo
+                    , Cuerpo
+                FROM Noticias
+                WHERE Id = {search_id}
+                """
 
-                csr_cacho.execute(sql_get)
-                resultado = csr_cacho.fetchone()
-                db_cacho.close()
+            csr_cacho.execute(sql_get)
+            resultado = csr_cacho.fetchone()
 
-                return resultado
-
-            except Exception as e:
-                db_cacho.close()
-                raise Exception(f"error al leer registros en tabla Noticias: {str(e)}")
+            return resultado
 
         except Exception as e:
-            raise Exception(f"error al abrir base de datos carro_maier: {str(e)}")
+            raise Exception(f"error al leer registros en tabla Noticias: {str(e)}")
+
+        finally:
+            db_cacho.close()
+
 
     def delete_data(self, search_id):
         """
@@ -174,20 +166,17 @@ class Model:
         try:
             db_cacho = sqlite3.connect('carro_maier.db')
 
-            try:
-                csr_cacho = db_cacho.cursor()
-                sql_delete = f"""
-                    DELETE FROM Noticias
-                    WHERE Id = {search_id}
-                    """
-                csr_cacho.execute(sql_delete)
-                db_cacho.commit()
-                db_cacho.close()
-
-            except Exception as e:
-                db_cacho.rollback()
-                db_cacho.close()
-                raise Exception(f"error al eliminar registro en tabla Noticias: {str(e)}")
+            csr_cacho = db_cacho.cursor()
+            sql_delete = f"""
+                DELETE FROM Noticias
+                WHERE Id = {search_id}
+                """
+            csr_cacho.execute(sql_delete)
+            db_cacho.commit()
 
         except Exception as e:
-            raise Exception(f"error al abrir base de datos carro_maier: {str(e)}")
+            db_cacho.rollback()
+            raise Exception(f"error al eliminar registro en tabla Noticias: {str(e)}")
+
+        finally:
+            db_cacho.close()
